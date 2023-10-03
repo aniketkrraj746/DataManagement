a6ce549b1c0a0b96916b3b40033c512e46c2caa4
aniketkrraj746 <aniketkrraj746@gmail.com>
2023-10-03 14:48:10 +0530

    index.js for static files

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ var cors = require("cors");
 const path = require("path");
 
 // static files
-app.use(express.static(path.join(__dirname,"/frontend/build")));
-app.get("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"/frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname,"/frontend/build")));
+// app.get("*",(req,res)=>{
+//   res.sendFile(path.join(__dirname,"/frontend/build/index.html"));
+// });
 const PORT = process.env.PORT || 4000;
-
-
-
 
 app.use(
   cors({
